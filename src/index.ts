882d387033eb29005f31f08703960ca06dd8e6b3
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-04-19 07:24:36 +0900

    [add]type example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello TS!!")
+// console.log("Hello TS!!")
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+type Age = number
+type Person = {
+    name: string
+    age: Age
+}
+
+let age: Age = 55
+let driver: Person = {
+    name: 'James May',
+    age: age
+}
+
+enum Language {
+    English = 0,
+    Spanish = 1,
+    Russian = 2
+}
+
+let myFirstLanguage = Language.Russian
+let mySecondLanguage = Language['English']
+
+let t = [true, false, true];
+
